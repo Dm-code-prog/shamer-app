d236704b3a877bcc5a306412e0e6b3957bd3a209
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-02-03 15:50:23 +0400

    Use inline keyboard for /start handler

--- app/api/webhook/telegram/route.ts
+++ app/api/webhook/telegram/route.ts
@@ -1,26 +1,44 @@
-import {NextRequest, NextResponse} from "next/server";
-import TelegramBot, {SendMessageOptions} from "node-telegram-bot-api";
+import { NextRequest, NextResponse } from 'next/server';
+import TelegramBot, { SendMessageOptions } from 'node-telegram-bot-api';
 
-
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN!, {webHook: true});
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN!, { webHook: true });
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
-  console.log("Received body:", body);
   const chatID = body.message.chat.id;
-  await startHandler(chatID);
-  return NextResponse.json({ok: true})
-}
 
+  try {
+    switch (body.message.text) {
+      case '/start':
+        await startHandler(chatID);
+        break;
+      default:
+        break;
+    }
+
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    return NextResponse.json({ ok: false });
+  }
+};
 
 const startHandler = async (chatID: string) => {
   const opts = {
     reply_markup: {
-      keyboard: [
-        [{web_app: {url: process.env.WEB_APP_URL!}, text: 'Open the Shamer app'}],
+      inline_keyboard: [
+        [
+          {
+            web_app: { url: process.env.WEB_APP_URL! },
+            text: 'Open the Shamer app',
+          },
+        ],
       ],
     },
-  } satisfies SendMessageOptions
-  
-  await bot.sendMessage(chatID, 'Shamer: Ignite your fitness journey with playful nudges and tough love. Celebrate progress, push limits, and remember—respect comes with dedication. Because, in the end, yes, you can. #RespectThroughChallenge', opts);
-}
+  } satisfies SendMessageOptions;
+
+  await bot.sendMessage(
+    chatID,
+    'Shamer: Ignite your fitness journey with playful nudges and tough love. Celebrate progress, push limits, and remember—respect comes with dedication. Because, in the end, yes, you can. #RespectThroughChallenge',
+    opts,
+  );
+};
