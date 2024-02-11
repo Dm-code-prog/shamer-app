975b226ffdc6f60f9515203ffdbac137a2c2e1d7
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-02-11 14:27:11 +0400

    Enable light mode support

--- components/ui/shamer-logo.tsx
+++ components/ui/shamer-logo.tsx
@@ -4,7 +4,7 @@ export const ShamerLogo = () => {
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-center text-5xl font-black">SHAMER</h1>
-      <p className="text-secondary cursor-text text-center text-sm">
+      <p className="text-muted-foreground cursor-text text-center text-sm">
         Use Shamer today to stay shameless forever.
       </p>
     </div>
