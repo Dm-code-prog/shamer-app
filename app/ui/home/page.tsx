1e125ddadb9e58904781e8f136eb2c6c938ff0f6
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-02-11 16:39:23 +0400

    fix

--- app/ui/home/page.tsx
+++ app/ui/home/page.tsx
@@ -1,19 +1,5 @@
 'use client';
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '#/components/ui/card';
-import { Input } from '#/components/ui/input';
-import { Button } from '#/components/ui/button';
-import { Avatar, AvatarFallback, AvatarImage } from 'components/ui/avatar';
-import { Progress } from 'components/ui/progress';
-import { DataTableDemo } from './challenge_table';
-import { HomeV0 } from '#/app/home/home-v0.0.1-alpha';
+import { HomeV0 } from './home-v0.0.1-alpha';
 
 export default function Page() {
   return (
