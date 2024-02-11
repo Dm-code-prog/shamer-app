6628e95339cbecff70c2601db941eb9caa515973
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-02-11 16:37:00 +0400

    Redirect to /ui/auth/telegram when cookie session is not present in the request

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,17 @@
 // This function can be marked `async` if using `await` inside
 import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/ui', request.nextUrl).toString());
+  }
+
+  const shamerSession = cookies().get('shamer_session');
+  if (!shamerSession) {
+    return NextResponse.redirect(
+      new URL('/ui/auth/telegram', request.nextUrl).toString(),
+    );
   }
 }
 
