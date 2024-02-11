588601d8e74d38cdf438e194bca739520730af14
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-02-11 15:23:59 +0400

    perform auth checks at /api/auth/telegram

--- app/api/auth/telegram/route.ts
+++ app/api/auth/telegram/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import crypto from 'crypto';
+
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
   if (!body.initData) {
@@ -13,11 +15,41 @@ export const POST = async (req: NextRequest) => {
 
   const { initData } = body;
 
-  console.log(initData, 'Telegram init data');
+  const params = new URLSearchParams(initData);
+  const hash = params.get('hash');
+
+  const fields = Array.from(params.keys()).filter((k) => k !== 'hash');
+
+  // Sort fields alphabetically and format them
+  const dataCheckString = fields
+    .sort()
+    .map((key) => `${key}=${params.get(key)}`)
+    .join('\n');
+
+  // Calculate the secret key
+  const secretKey = crypto
+    .createHmac('sha256', 'WebAppData')
+    .update(process.env.TELEGRAM_TOKEN!)
+    .digest();
+
+  // Calculate HMAC-SHA-256 signature of the data-check-string with the secret key
+  const hmac = crypto
+    .createHmac('sha256', secretKey)
+    .update(dataCheckString)
+    .digest('hex');
+  if (hmac !== hash) {
+    return NextResponse.json(
+      {
+        error: 'Unauthorized',
+      },
+      { status: 401 },
+    );
+  }
   return NextResponse.json(
     {
-      error: 'Unauthorized',
+      ok: true,
+      message: 'Telegram user authorized',
     },
-    { status: 401 },
+    { status: 200 },
   );
 };
