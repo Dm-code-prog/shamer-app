4c5c5fa0de5bc735661b3916765c12a2676e6cf7
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-03-03 22:39:57 +0400

    fix

--- domains/user/server/user_info.ts
+++ domains/user/server/user_info.ts
@@ -2,7 +2,9 @@ import 'server-only';
 import { sql } from '@vercel/postgres';
 import { UserInfo } from '#/domains/user/types';
 
-export const setUserInfo = async (u: UserInfo): Promise<void> => {
+export const setUserInfo = async (
+  u: UserInfo & { user_id: string },
+): Promise<void> => {
   await sql`insert into user_info (user_id, weight, height, age)
             values (${u.user_id}, ${u.weight}, ${u.height}, ${u.age})
             on conflict (user_id) do update set weight = ${u.weight},
