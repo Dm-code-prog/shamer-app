6ed74ff6767ba29cf4ce05346836ec4f88665785
Dmitry Kozlov <dmitry.kozlov@armenotech.com>
2024-04-01 14:58:04 +0400

    Update user_rp, team_rp materialized views

--- domains/challenge/sql/team_rp.sql
+++ domains/challenge/sql/team_rp.sql
@@ -1,13 +1,19 @@
 create materialized view team_rp as
-select t.id, round(sum(at.met * 70 * ca.time * coalesce(ca.weight_coefficient, 1) * coalesce(at.base_weight, 1)) /
-             10) as total_rp
+select t.id,
+       round(sum(((((10 * ui.weight) + (6.25 * ui.height) - (5 * ui.age) + 5) * 1.55 / 24) * at.met * ca.time) /
+           10)) as total_rp
 from teams t
          join challenges c on t.id = c.team_id
          join challenge_instances ci on c.id = ci.challenge_id
          join user_stats us on ci.id = us.challenge_instance_id
+         join user_info ui on us.user_id = ui.user_id
          join challenge_activities ca on us.challenge_activity_id = ca.id
          join activity_types at on ca.activity_type_id = at.id
 group by t.id;
+
+
+drop materialized view team_rp;
+
 
 -- Create a trigger function to refresh the materialized view
 CREATE OR REPLACE FUNCTION refresh_team_rp()
@@ -25,3 +31,5 @@ CREATE TRIGGER refresh_team_rp_trigger
     ON user_stats
     FOR EACH STATEMENT
 EXECUTE FUNCTION refresh_team_rp();
+
+drop trigger refresh_team_rp_trigger on user_stats;
--- domains/challenge/sql/user_rp.sql
+++ domains/challenge/sql/user_rp.sql
@@ -1,13 +1,14 @@
 create materialized view user_rp as
 select us.user_id,
-       round(sum(at.met * 70 * ca.time * coalesce(ca.weight_coefficient, 1) * coalesce(at.base_weight, 1)) /
-             10) as total_rp
+      round( sum(((((10 * ui.weight) + (6.25 * ui.height) - (5 * ui.age) + 5) * 1.55 / 24) * at.met * ca.time) /
+           10)) as total_rp
 from user_stats us
+         left join user_info ui on us.user_id = ui.user_id
          left join challenge_activities ca on us.challenge_activity_id = ca.id
          left join activity_types at on ca.activity_type_id = at.id
 group by us.user_id;
 
-
+drop materialized view if exists user_rp;
 
 -- Create a trigger function to refresh the materialized view
 CREATE OR REPLACE FUNCTION refresh_user_rp()
@@ -25,3 +26,6 @@ CREATE TRIGGER refresh_user_rp_trigger
     ON user_stats
     FOR EACH STATEMENT
 EXECUTE FUNCTION refresh_user_rp();
+
+
+drop trigger if exists refresh_user_rp_trigger on user_stats;
